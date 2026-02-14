2eec434c52211215255fa9a0aca7dd695a45b715
e-rinfn <erinfajrinnugraha@gmail.com>
2026-02-14 10:05:48 +0800

    perbaikan pada base url

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__ . '/config/config.php';
+
 // Redirect ke halaman dashboard
-header("Location: " . BASE_URL . "/modules/dashboard/");
+header("Location: " . BASE_URL . "modules/dashboard/");
 exit();
